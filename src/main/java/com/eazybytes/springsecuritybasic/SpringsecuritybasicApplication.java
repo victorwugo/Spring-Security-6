b334e45ead76ada8823d4990ac0cdaf4002d10f4
Victor Wu <victorwu@Victors-MacBook-Pro.local>
2024-06-03 20:11:35 -0700

    Create api with no security

--- src/main/java/com/eazybytes/springsecuritybasic/SpringsecuritybasicApplication.java
+++ src/main/java/com/eazybytes/springsecuritybasic/SpringsecuritybasicApplication.java
@@ -2,8 +2,12 @@ package com.eazybytes.springsecuritybasic;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+// If controller is created outside of parent package(com.eazybytes.springsecuritybasic)
+// Just an example in this case
+@ComponentScan("com.eazybytes.springsecuritybasic.controller")
 public class SpringsecuritybasicApplication {
 
 	public static void main(String[] args) {
