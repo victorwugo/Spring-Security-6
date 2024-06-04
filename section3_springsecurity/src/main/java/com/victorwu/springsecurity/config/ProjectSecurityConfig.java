8e3b7289a41f07ff8e94a65824e8e426118bf88b
Victor Wu <victorwu@Victors-MacBook-Pro.local>
2024-06-04 15:10:35 -0700

    S3.23 Create InMemory Users with  method

--- section3_springsecurity/src/main/java/com/victorwu/springsecurity/config/ProjectSecurityConfig.java
+++ section3_springsecurity/src/main/java/com/victorwu/springsecurity/config/ProjectSecurityConfig.java
@@ -3,6 +3,9 @@ package com.victorwu.springsecurity.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 import static org.springframework.security.config.Customizer.withDefaults;
@@ -19,6 +22,24 @@ public class ProjectSecurityConfig {
                 .httpBasic(withDefaults());
 
         return http.build();
+    }
+
+    @Bean
+    public InMemoryUserDetailsManager userDetailsService() {
+        // `withDefaultPasswordEncoder()` method approach for creating user details
+        UserDetails admin = User.withDefaultPasswordEncoder()
+                .username("admin")
+                .password("12345")
+                .authorities("admin")
+                .build();
+
+        UserDetails user = User.withDefaultPasswordEncoder()
+                .username("user")
+                .password("12345")
+                .authorities("read")
+                .build();
+
+        return new InMemoryUserDetailsManager(admin, user);
     }
 
 }
